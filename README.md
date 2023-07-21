787b7067386292e297b5ae2778e77750da192a91
Trewaters <trewaters@hotmail.com>
2023-07-21 16:43:43 -0700

    options reads poses

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Soar
+
+Found yoga poses [here](https://www.pocketyoga.com/pose/)
+
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
 import MenuMain from './component/MenuMain'
+import MenuPlanner from './component/MenuPlanner'
+import PostureSearch from '../pages/components/PosturesSearch'
 
 export default function Home() {
   return (
     <>
     <MenuMain />
+    <MenuPlanner />
+    <PostureSearch />
     </>
   )
 }
