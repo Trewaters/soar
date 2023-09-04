46a71cc034874ac2898caa663540a5138b9ac1ed
Tre' Grisby <Trewaters@users.noreply.github.com>
2023-09-04 16:18:14 -0700

    fixed small head error

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head';
 import LandingPage from '../components/LandingPage'
 import { Box, Paper } from '@mui/material';
 import RootLayout from '@/app/layout';
-
-
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Happy Yoga &quot;Soar&quot;',
+}
+ 
 
 export default function Home() {
 
 
   return (
     <RootLayout>
-      <Head>
-        <title>Happy Yoga &quot;Soar&quot;</title>
-      </Head>
       <Box sx={{ display: 'flex', justifyContent: 'center', bgcolor: 'lightgray' }}>
         <Paper>
           <LandingPage />
