2773555eaf7977a4dac2c5e2363d08a4e09c9ef7
Trewaters <trewaters@hotmail.com>
2024-01-20 14:13:50 -0800

    ADDED Metadata back in

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import { ComponentType } from 'react'
-// import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 import { ThemeProvider } from '@mui/material/styles'
 import { theme } from '@styles/theme'
 import RootLayout from '@/app/layout'
 
-// export const metadata: Metadata = {
-//   title: 'Happy Yoga',
-//   description: 'Soar like a leaf on the wind!',
-// }
+export const metadata: Metadata = {
+  title: 'Happy Yoga',
+  description: 'Soar like a leaf on the wind!',
+}
 
 function MyApp({
   Component,
