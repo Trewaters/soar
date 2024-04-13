122fc55e3fcae1354fd6c783393453eb632a9c1f
Trewaters <trewaters@hotmail.com>
2024-04-12 20:53:48 -0700

    ADDED: 1st draft of postures

--- app/flowSeries/practice/page.tsx
+++ app/flowSeries/practice/page.tsx
@@ -1,3 +1,30 @@
+'use client'
+import { Card, CardContent, Stack, Typography } from '@mui/material'
+
+const flowSeries = [
+  'Mountain Pose',
+  'Downward Dog',
+  'Warrior 1',
+  'Warrior 2',
+  'Triangle Pose',
+  'Tree Pose',
+  "Child's Pose",
+  'Corpse Pose',
+]
+
 export default function Page() {
-  return <>Flow Series Practice</>
+  return (
+    <>
+      <Typography>Flow Series Practice</Typography>
+      <Stack>
+        {flowSeries.map((pose) => (
+          <Card key={pose}>
+            <CardContent>
+              <Typography key={pose}>{pose}</Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Stack>
+    </>
+  )
 }
