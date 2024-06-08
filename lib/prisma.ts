e5d885afe79e215e85a41e22f7b5593b9289b24a
Trewaters <trewaters@hotmail.com>
2024-06-08 09:51:38 -0700

    fix build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prismaSchema/client'
+
+import { PrismaClient } from '@prisma/generated/client'
 
 const prisma = new PrismaClient()
 
