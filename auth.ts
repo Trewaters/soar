77817e165ca53b609cd39bf4615ebd263e750a99
Trewaters <trewaters@hotmail.com>
2024-06-30 20:36:28 -0700

    ADDED: mongoDb adapter.

--- auth.ts
+++ auth.ts
@@ -1,22 +1,33 @@
 import NextAuth from 'next-auth'
-
 import type { NextAuthConfig } from 'next-auth'
-// import { type Adapter } from '@auth/core/adapters'
-// import { PrismaAdapter } from '@auth/prisma-adapter'
-// import { PrismaClient } from '@prisma/generated/client'
+import { type Adapter } from '@auth/core/adapters'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import authConfig from '@auth.config'
+import clientPromise from '@lib/db'
 
-// const prisma = new PrismaClient()
+/*  
+Resource Links:
 
-// const MyAdapter: Adapter = {
-// ...PrismaAdapter(prisma),
-// Add your custom methods here
-// }
+- https://authjs.dev/getting-started/adapters/prisma
+- https://www.prisma.io/docs/orm/overview/databases/mongodb
+- https://www.prisma.io/docs/getting-started/setup-prisma/start-from-scratch/mongodb/connect-your-database-typescript-mongodb
+- https://authjs.dev/getting-started/adapters/mongodb
+- https://authjs.dev/getting-started/migrating-to-v5#authenticating-server-side
+- https://authjs.dev/reference/core/adapters#adapter
+- https://authjs.dev/reference/nextjs/jwt
+- https://authjs.dev/reference/mongodb-adapter
+
+* example app using mongodb adapter:
+- https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+*/
+
+const MyAdapter: Adapter = {
+  ...MongoDBAdapter(clientPromise),
+}
 
 const config = {
   theme: { logo: 'https://authjs.dev/img/logo-sm.png' },
-  // adapter: PrismaAdapter(prisma),
-  // adapter: MyAdapter,
+  adapter: MyAdapter,
   basePath: '/auth',
   callbacks: {
     authorized({ request, auth }) {
