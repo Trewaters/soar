0da3706f4e6810023e364d0cb0ed399cac8526bb
Trewaters <trewaters@hotmail.com>
2024-07-20 06:25:30 -0700

    FIX: troubleshooting atlas issues

--- auth.ts
+++ auth.ts
@@ -41,44 +41,54 @@ const config = {
       const email = user.email
 
       // Check if the user already exists
-      // const existingUser = await prisma.user.findUnique({
-      //   where: { email: email ?? undefined },
-      // })
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: { email: email ?? undefined },
+        })
+        if (existingUser) {
+          console.log('User already exists:', existingUser)
+          return true
+        }
+      } catch (error) {
+        console.error('Error finding user:', error)
+        throw error
+      }
+
       // console.log('existingUser:', existingUser)
       console.log('account:', account)
 
       // if (!existingUser && account) {
-      // if (user && account) {
-      //   try {
-      //     const newUser = await prisma.user.create({
-      //       data: {
-      //         provider_id: user.id,
-      //         name: user.name,
-      //         email: user.email,
-      //         image: user.id,
-      //         profile: JSON.stringify(profile),
-      //         accounts: {
-      //           create: {
-      //             provider: account.provider,
-      //             providerAccountId: account.providerAccountId,
-      //             refresh_token: account.refresh_token ?? undefined,
-      //             access_token: account.access_token ?? undefined,
-      //             expires_at: account.expires_at ?? undefined,
-      //             token_type: account.token_type ?? undefined,
-      //             scope: account.scope ?? undefined,
-      //             id_token: account.id_token ?? undefined,
-      //             session_state: JSON.stringify(account.session_state),
-      //             type: account.type,
-      //           },
-      //         },
-      //       },
-      //     })
-      //     console.log('New user created:', newUser)
-      //   } catch (error) {
-      //     console.error('Error creating new user:', error)
-      //     throw error
-      //   }
-      // }
+      if (user && account) {
+        try {
+          const newUser = await prisma.user.create({
+            data: {
+              provider_id: user.id,
+              name: user.name,
+              email: user.email,
+              image: user.id,
+              profile: JSON.stringify(profile),
+              accounts: {
+                create: {
+                  provider: account.provider,
+                  providerAccountId: account.providerAccountId,
+                  refresh_token: account.refresh_token ?? undefined,
+                  access_token: account.access_token ?? undefined,
+                  expires_at: account.expires_at ?? undefined,
+                  token_type: account.token_type ?? undefined,
+                  scope: account.scope ?? undefined,
+                  id_token: account.id_token ?? undefined,
+                  session_state: JSON.stringify(account.session_state),
+                  type: account.type,
+                },
+              },
+            },
+          })
+          console.log('New user created:', newUser)
+        } catch (error) {
+          console.error('Error creating new user:', error)
+          throw error
+        }
+      }
       return true
     },
     async session({ session, token }) {
