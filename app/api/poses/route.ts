7c4a7195e88cce23eb0665304c59bb6c43b6ef76
Trewaters <trewaters@hotmail.com>
2024-08-08 20:53:58 -0700

    Notes for later

--- app/api/poses/route.ts
+++ app/api/poses/route.ts
@@ -2,25 +2,6 @@ import { NextResponse } from 'next/server'
 import postureData from '@interfaces/postureData'
 import { PrismaClient } from '@prisma/generated/client'
 
-// export async function GET() {
-//   const url = 'https://www.pocketyoga.com/poses.json'
-//   try {
-//     const response = await fetch(url)
-//     if (!response.ok) {
-//       throw new Error(`Error fetching data: ${response.status}`)
-//     }
-//     const data = await response.json()
-//     const dataWithId: postureData[] = data.map(
-//       (item: postureData, index: number) => ({
-//         ...item,
-//         id: index + 1,
-//       })
-//     )
-//     return NextResponse.json(dataWithId)
-//   } catch (error: any) {
-//     return NextResponse.json({ error: error.message }, { status: 500 })
-//   }
-// }
 const prisma = new PrismaClient()
 
 export async function GET() {
@@ -38,3 +19,25 @@ export async function GET() {
     await prisma.$disconnect()
   }
 }
+
+// route pulls from alternate web source
+/* export async function GET() {
+  const url = 'https://www.pocketyoga.com/poses.json'
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.status}`)
+    }
+    const data = await response.json()
+    const dataWithId: postureData[] = data.map(
+      (item: postureData, index: number) => ({
+        ...item,
+        id: index + 1,
+      })
+    )
+    return NextResponse.json(dataWithId)
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
+ */
