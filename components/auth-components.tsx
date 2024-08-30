c4c131f0a85102ccdf2eb6454ca81b8624457e2a
Trewaters <trewaters@hotmail.com>
2024-08-30 15:57:26 -0700

    UPDATE: Ui improvements

--- components/auth-components.tsx
+++ components/auth-components.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { Button } from '@mui/material'
+import { Button, Stack } from '@mui/material'
 import { signIn, signOut } from '@/auth'
 
-export default function SignIn({
+export default async function SignIn({
   provider,
   ...props
 }: { provider?: string } & React.ComponentPropsWithRef<typeof Button>) {
   return (
-    <form
-      action={async () => {
-        'use server'
-        await signIn(provider)
-      }}
-    >
-      <Button type="submit" variant="contained" {...props}>
-        Sign In
-      </Button>
-    </form>
+    <Stack textAlign={'center'} spacing={2} sx={{ my: 6 }}>
+      <form
+        action={async () => {
+          'use server'
+          await signIn(provider)
+        }}
+      >
+        <Button type="submit" variant="contained" {...props}>
+          Sign In
+        </Button>
+      </form>
+    </Stack>
   )
 }
 
-export function SignOut(props: React.ComponentPropsWithRef<typeof Button>) {
+export async function SignOut(
+  props: React.ComponentPropsWithRef<typeof Button>
+) {
   return (
     <form
       action={async () => {
--- components/user-button.tsx
+++ components/user-button.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth'
 import SignIn, { SignOut } from './auth-components'
+import { Box, Typography } from '@mui/material'
 
 export default async function UserButton() {
   /*  
@@ -8,10 +9,28 @@ export default async function UserButton() {
   */
   const session = await auth()
 
-  if (!session?.user) return <SignIn />
+  if (!session?.user)
+    return (
+      <>
+        <SignIn />
+        {!session?.user && (
+          <Box sx={{ pt: 4 }} display={'flex'} flexDirection={'column'}>
+            <Typography variant="h2">Good Bye 👋🏾</Typography>
+            <Typography variant="h2">or</Typography>
+            <Typography variant="h2">sign in again 🔓</Typography>
+          </Box>
+        )}
+      </>
+    )
   return (
     <>
       <SignOut />
+      {session?.user && (
+        <Box sx={{ pt: 4 }}>
+          <Typography variant="h2">Welcome 🔆</Typography>
+          <Typography variant="h2">{session.user.name}</Typography>
+        </Box>
+      )}
     </>
   )
 }
