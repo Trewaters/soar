3d17f450cc88c9373d2b553904bcc4d51fd7e3b2
Trewaters <trewaters@hotmail.com>
2024-09-22 10:28:55 -0700

    ADD: bottom menu icons

--- components/navBottom.tsx
+++ components/navBottom.tsx
@@ -1,14 +1,49 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material'
+'use client'
+import { IconButton, Toolbar } from '@mui/material'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function NavBottom() {
+  const router = useRouter()
   return (
     <>
       <Toolbar
         sx={{ backgroundColor: 'white', alignSelf: 'center', borderRadius: 2 }}
       >
-        <IconButton disableRipple>Home</IconButton>
-        <IconButton disableRipple>User</IconButton>
-        <IconButton disableRipple>Bottom Burger</IconButton>
+        <IconButton
+          disableRipple
+          onClick={() => {
+            router.push('/')
+          }}
+        >
+          <Image
+            alt="Home icon"
+            src="/icons/bottom-home.svg"
+            width={20}
+            height={20}
+          />
+        </IconButton>
+        <IconButton
+          disableRipple
+          onClick={() => {
+            router.push('/navigator/profile')
+          }}
+        >
+          <Image
+            alt="User profile icon"
+            src="/icons/bottom-user.svg"
+            width={20}
+            height={20}
+          />
+        </IconButton>
+        <IconButton disableRipple disabled>
+          <Image
+            alt="Bottom burger menu icon"
+            src="/icons/bottom-burger-menu.svg"
+            width={20}
+            height={20}
+          />
+        </IconButton>
       </Toolbar>
     </>
   )
