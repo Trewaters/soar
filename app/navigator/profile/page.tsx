05b046d03dcf5ebf5d08ed3fb2c6eeb1bbd303a9
Trewaters <trewaters@hotmail.com>
2024-11-08 20:31:13 -0800

    sign in options for profile page

--- app/navigator/profile/page.tsx
+++ app/navigator/profile/page.tsx
@@ -1,14 +1,32 @@
 import { auth } from '@auth'
-import { Box } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import { SessionProvider } from 'next-auth/react'
 import UserDetails from './UserDetails'
+import SignIn, { SignOut } from '@serverComponents/auth-components'
 
 export default async function Page() {
   const session = await auth()
   return (
-    <Box display={'flex'} justifyContent={'center'} marginTop={4}>
+    <Box
+      display={'flex'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      flexDirection={'column'}
+      marginTop={4}
+    >
       <SessionProvider basePath={'/auth'} session={session}>
-        <UserDetails />
+        <Stack>
+          <UserDetails />
+        </Stack>
+        {session ? (
+          <Stack>
+            <SignOut />
+          </Stack>
+        ) : (
+          <Stack>
+            <SignIn />
+          </Stack>
+        )}
       </SessionProvider>
     </Box>
   )
