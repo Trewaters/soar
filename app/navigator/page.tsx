df33a3a21bd7296e4f1cd0d67ee2798595a94afe
Tre' Grisby <Trewaters@users.noreply.github.com>
2025-05-03 09:11:01 -0700

    ADD: Added yogi image to splash page

--- app/navigator/page.tsx
+++ app/navigator/page.tsx
@@ -1,6 +1,7 @@
 // import CurrentTime from '@app/clientComponents/current-time'
 import TabHeader from '@app/clientComponents/tab-header'
 import { Box, Typography } from '@mui/material'
+import Image from '@node_modules/next/image'
 
 export default function Page() {
   return (
@@ -11,6 +12,12 @@ export default function Page() {
         role="main"
         aria-labelledby="page-title"
       >
+        <Image
+          src={'/images/primary/Home-page-yogi.png'}
+          alt="Home page yogi"
+          width={200}
+          height={200}
+        />
         {/* <Typography variant="body1" id="page-title">
           Like a leaf on the Wind
         </Typography> */}
