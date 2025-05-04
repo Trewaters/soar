684bfc7b84924a8c163f2265f0f4d5f6f8e30a71
Trewaters <trewaters@hotmail.com>
2025-05-04 14:44:29 -0700

    EDIT: enable timer. update colors for splash page.

--- app/navigator/page.tsx
+++ app/navigator/page.tsx
@@ -1,4 +1,4 @@
-// import CurrentTime from '@app/clientComponents/current-time'
+import CurrentTime from '@app/clientComponents/current-time'
 import TabHeader from '@app/clientComponents/tab-header'
 import { Box, Typography } from '@mui/material'
 import Image from '@node_modules/next/image'
@@ -14,17 +14,14 @@ export default function Page() {
       >
         <Image
           src={'/images/primary/Home-page-yogi.png'}
-          alt="Home page yogi"
-          width={200}
-          height={200}
+          alt="Like a leaf on the Wind"
+          width={207}
+          height={207}
         />
-        {/* <Typography variant="body1" id="page-title">
-          Like a leaf on the Wind
-        </Typography> */}
-        <Typography variant="body1" id="page-title">
-          a yoga exercise app
+        <Typography variant="body1" id="page-title" color="primary.main">
+          yoga exercise
         </Typography>
-        {/* <CurrentTime /> */}
+        <CurrentTime />
       </Box>
       <TabHeader />
     </>
