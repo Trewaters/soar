26db84e63299811596edd183519c95d70bb13c9b
Trewaters <trewaters@hotmail.com>
2025-05-15 20:10:28 -0700

    fix favicon

--- components/header.tsx
+++ components/header.tsx
@@ -79,15 +79,20 @@ export default function Header() {
   }
 
   const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <nav aria-label="main navigation menu">
+    <nav aria-label="main navigation menu">
+      <Box
+        sx={{ width: 250 }}
+        role="presentation"
+        onClick={toggleDrawer(false)}
+      >
         <List>
           {navLinks.map((navItem, index) => (
             <React.Fragment key={navItem.name}>
               {index === navLinks.length - 1 && <Divider />}
               <ListItem disablePadding>
-                <Link href={navItem.href} passHref>
+                <Link href={navItem.href} passHref legacyBehavior>
                   <ListItemButton
+                    component="button"
                     sx={{ width: '250px' }}
                     onClick={() => {
                       setOpenDrawer(false)
@@ -118,8 +123,8 @@ export default function Header() {
             </React.Fragment>
           ))}
         </List>
-      </nav>
-    </Box>
+      </Box>
+    </nav>
   )
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -128,81 +133,58 @@ export default function Header() {
   }
 
   return (
-    <AppBar
-      position="static"
-      sx={{
-        pr: 2,
-        height: '69px',
-      }}
-      elevation={0}
-    >
-      <Box
+    <header>
+      <AppBar
+        position="static"
         sx={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: '100%',
+          pr: 2,
+          height: '69px',
         }}
+        elevation={0}
       >
-        <Stack direction="row" justifyContent={'space-between'}>
-          <IconButton
-            disableRipple
-            onClick={toggleDrawer(true)}
-            aria-label="Open main navigation"
-            aria-controls="main-navigation"
-          >
-            <MenuIcon sx={{ height: '2em', width: '2em' }} />
-          </IconButton>
-          <Box
-            sx={{
-              position: 'absolute',
-              left: '50%',
-              transform: 'translateX(-50%)',
-            }}
-          >
-            <Link href="/" passHref legacyBehavior>
-              {/* <Button
-                sx={{
-                  background: 'none',
-                  border: 'none',
-                  padding: 0,
-                  margin: 0,
-                  textDecoration: 'none',
-                  color: 'inherit',
-                  cursor: 'pointer',
-                }}
-                onClick={() => {
-                  router.push('/')
-                }}
-              >
-                <Typography
-                  variant="h3"
-                  sx={{
-                    pt: 3,
-                    textDecoration: 'none',
-                    textTransform: 'uppercase',
-                    '& :hover:': { textDecoration: 'none' },
-                  }}
-                >
-                  Soar
-                </Typography>
-              </Button> */}
-              <Image
-                src="/logo/Main Logo in Contrast Light150px.png"
-                alt="Soar Logo"
-                width="150"
-                height="20"
-                style={{ marginTop: '15%' }}
-              />
-            </Link>
-          </Box>
-        </Stack>
-      </Box>
-      <Drawer open={openDrawer} onClose={toggleDrawer(false)}>
-        <Paper onClick={handleClick} sx={{ height: '100%' }}>
-          {DrawerList}
-        </Paper>
-      </Drawer>
-    </AppBar>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Stack direction="row" justifyContent={'space-between'}>
+            <IconButton
+              disableRipple
+              onClick={toggleDrawer(true)}
+              aria-label="Open main navigation"
+              aria-controls="main-navigation"
+              component="button"
+            >
+              <MenuIcon sx={{ height: '2em', width: '2em' }} />
+            </IconButton>
+            <Box
+              sx={{
+                position: 'absolute',
+                left: '50%',
+                transform: 'translateX(-50%)',
+              }}
+            >
+              <Link href="/" passHref legacyBehavior>
+                <Image
+                  src="/logo/Main Logo in Contrast Light150px.png"
+                  alt="Soar Yoga main logo"
+                  width={150}
+                  height={20}
+                  style={{ marginTop: '15%' }}
+                />
+              </Link>
+            </Box>
+          </Stack>
+        </Box>
+        <Drawer open={openDrawer} onClose={toggleDrawer(false)}>
+          <Paper onClick={handleClick} sx={{ height: '100%' }}>
+            {DrawerList}
+          </Paper>
+        </Drawer>
+      </AppBar>
+    </header>
   )
 }
