e5c26f3e30a2bee0867db00dbd4b16bf52c1f3fd
Trewaters <trewaters@hotmail.com>
2025-06-10 15:40:01 -0700

    UPDATE: prisma client

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -215,7 +215,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\trewa\\Documents\\GitHub\\soar\\prisma\\generated\\client",
+      "value": "C:\\Users\\trewa\\Documents\\Github\\NextJS tutorials\\soar\\prisma\\generated\\client",
       "fromEnvVar": null
     },
     "config": {
@@ -233,7 +233,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\trewa\\Documents\\GitHub\\soar\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\trewa\\Documents\\Github\\NextJS tutorials\\soar\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
