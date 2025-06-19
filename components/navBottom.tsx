e962e50b9c267c57118c1050ff1dd505191f8e22
Trewaters <trewaters@hotmail.com>
2025-06-19 11:06:16 -0700

    EDIT: add dynamic styles to nav bottom based on user login status.

--- components/navBottom.tsx
+++ components/navBottom.tsx
@@ -1,10 +1,62 @@
 'use client'
-import React, { AppBar, IconButton } from '@mui/material'
-import Image from 'next/image'
+import React from 'react'
+import { AppBar, IconButton } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import PersonIcon from '@mui/icons-material/Person'
+import MenuIcon from '@mui/icons-material/Menu'
 import { useRouter } from 'next/navigation'
+import { useSession } from 'next-auth/react'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type ColorFunction = (isAuthenticated: boolean) => string
+
+interface NavItem {
+  id: string
+  label: string
+  icon: React.ReactNode
+  path: string | (() => string)
+  getColor: ColorFunction
+}
 
 export default function NavBottom(props: { subRoute: string }) {
   const router = useRouter()
+  const { data: session, status } = useSession()
+
+  // Determine if user is authenticated
+  const isAuthenticated = status === 'authenticated' && !!session
+
+  const navItems: NavItem[] = [
+    {
+      id: 'home',
+      label: 'Navigate to home page',
+      icon: <HomeIcon />,
+      path: '/',
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      getColor: (_isAuthenticated: boolean) => 'primary.main', // Always primary.main
+    },
+    {
+      id: 'profile',
+      label: 'Navigate to user profile',
+      icon: <PersonIcon />,
+      path: '/navigator/profile',
+      getColor: (isAuthenticated: boolean) =>
+        isAuthenticated ? 'success.main' : 'grey.500', // Green when logged in, gray when logged out
+    },
+    {
+      id: 'menu',
+      label: 'Open navigation menu',
+      icon: <MenuIcon />,
+      path: () => props.subRoute,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      getColor: (_isAuthenticated: boolean) => 'primary.contrastText', // Always primary.contrastText
+    },
+  ]
+
+  const handleNavigation = (path: string | (() => string)) => {
+    const targetPath = typeof path === 'function' ? path() : path
+    router.push(targetPath)
+  }
+
   return (
     <AppBar
       component="nav"
@@ -25,69 +77,30 @@ export default function NavBottom(props: { subRoute: string }) {
         justifySelf: 'center',
       }}
     >
-      <IconButton
-        disableRipple
-        aria-label="Navigate to home page"
-        onClick={() => {
-          router.push('/')
-        }}
-        sx={{
-          '&:focus': {
-            outline: '2px solid',
-            outlineColor: 'primary.main',
-            outlineOffset: '2px',
-          },
-        }}
-      >
-        <Image
-          alt="Home icon"
-          src="/icons/bottom-home.svg"
-          width={20}
-          height={20}
-        />
-      </IconButton>
-      <IconButton
-        disableRipple
-        aria-label="Navigate to user profile"
-        onClick={() => {
-          router.push('/navigator/profile')
-        }}
-        sx={{
-          '&:focus': {
-            outline: '2px solid',
-            outlineColor: 'primary.main',
-            outlineOffset: '2px',
-          },
-        }}
-      >
-        <Image
-          alt="User profile icon"
-          src="/icons/bottom-user.svg"
-          width={20}
-          height={20}
-        />
-      </IconButton>
-      <IconButton
-        disableRipple
-        aria-label="Open navigation menu"
-        onClick={() => {
-          router.push(props.subRoute)
-        }}
-        sx={{
-          '&:focus': {
-            outline: '2px solid',
-            outlineColor: 'primary.main',
-            outlineOffset: '2px',
-          },
-        }}
-      >
-        <Image
-          alt="Bottom burger menu icon"
-          src="/icons/bottom-burger-menu.svg"
-          width={20}
-          height={20}
-        />
-      </IconButton>
+      {navItems.map((item) => (
+        <IconButton
+          key={item.id}
+          disableRipple
+          disabled={item.id === 'profile' && !isAuthenticated} // Disable profile when not authenticated
+          aria-label={item.label}
+          onClick={() => handleNavigation(item.path)}
+          sx={{
+            color: item.getColor(isAuthenticated),
+            '&:focus': {
+              outline: '2px solid',
+              outlineColor: 'primary.main',
+              outlineOffset: '2px',
+            },
+            '&.Mui-disabled': {
+              color: item.getColor(isAuthenticated), // Use dynamic color even when disabled
+            },
+          }}
+        >
+          {React.cloneElement(item.icon as React.ReactElement, {
+            sx: { color: item.getColor(isAuthenticated) },
+          })}
+        </IconButton>
+      ))}
     </AppBar>
   )
 }
