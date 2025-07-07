cc2e3ac1884abee2bd51ef513e6a032e7da11023
Tre' Grisby <Trewaters@users.noreply.github.com>
2025-07-07 20:21:06 +0200

    EDIT: conform to figma for sign in page.

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -4,6 +4,7 @@ import Header from '@serverComponents/header'
 import Image from 'next/image'
 import { signIn, providerMap, signOut, auth } from '../../../auth'
 import { redirect } from 'next/navigation'
+import { Link as MuiLink } from '@mui/material'
 import Link from 'next/link'
 import CredentialsInput from './credentialsInput'
 
@@ -32,111 +33,118 @@ export default async function SignInPage(props: {
             justifyContent: 'space-between',
           }}
         >
-          <Stack spacing={1} alignItems={'center'}>
+          <Stack spacing={3} alignItems={'center'}>
             {session ? (
               <>
                 <Typography variant="h2" color="success.main">
                   You&apos;re signed in!
                 </Typography>
                 <Typography variant="body1">
-                  <Link href="/">Click here</Link>
+                  <Link href="/" passHref legacyBehavior>
+                    <MuiLink>Click here</MuiLink>
+                  </Link>
                   &nbsp;to go to the home page.
                 </Typography>
+                <Paper
+                  component="form"
+                  action={async () => {
+                    'use server'
+                    await signOut({
+                      redirect: true,
+                      redirectTo: '/auth/signout',
+                    })
+                  }}
+                  elevation={0}
+                  sx={{ backgroundColor: 'transparent' }}
+                >
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    sx={{ width: 'auto' }}
+                  >
+                    Sign out
+                  </Button>
+                </Paper>
               </>
             ) : (
-              <>
+              <Stack alignItems={'center'} spacing={2}>
                 <Typography variant="h2">Welcome back.</Typography>
                 <Typography variant="body1">
                   We&apos;re happy you&apos;re here!
                 </Typography>
-              </>
-            )}
-          </Stack>
+                {Object.values(providerMap).map((provider, index) => (
+                  <Paper
+                    key={index}
+                    component="form"
+                    action={async () => {
+                      'use server'
+                      // eslint-disable-next-line no-useless-catch
+                      try {
+                        await signIn(provider.id, {
+                          redirectTo:
+                            props.searchParams?.callbackUrl ?? '/navigator',
+                        })
+                      } catch (error) {
+                        // Signin can fail for a number of reasons, such as the user
+                        // not existing, or the user not having the correct role.
+                        // In some cases, you may want to redirect to a custom error
+                        if (
+                          error &&
+                          typeof error === 'object' &&
+                          'type' in error
+                        ) {
+                          return redirect(
+                            `${process.env.SIGNIN_ERROR_URL}?error=${error.type}`
+                          )
+                        }
 
-          <Stack alignItems={'center'} spacing={2}>
-            {Object.values(providerMap).map((provider, index) => (
-              <Paper
-                key={index}
-                component="form"
-                action={async () => {
-                  'use server'
-                  // eslint-disable-next-line no-useless-catch
-                  try {
-                    await signIn(provider.id, {
-                      redirectTo:
-                        props.searchParams?.callbackUrl ?? '/navigator',
-                    })
-                  } catch (error) {
-                    // Signin can fail for a number of reasons, such as the user
-                    // not existing, or the user not having the correct role.
-                    // In some cases, you may want to redirect to a custom error
-                    if (error && typeof error === 'object' && 'type' in error) {
-                      return redirect(
-                        `${process.env.SIGNIN_ERROR_URL}?error=${error.type}`
-                      )
-                    }
-
-                    // Otherwise if a redirects happens Next.js can handle it
-                    // so you can just re-thrown the error and let Next.js handle it.
-                    // Docs:
-                    // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
-                    throw error
-                  }
-                }}
-                elevation={0}
-                sx={{ backgroundColor: 'transparent' }}
-              >
-                <Button
-                  type="submit"
-                  variant="outlined"
-                  sx={{ m: 2, borderRadius: '12px' }}
-                  startIcon={
-                    <Image
-                      src={
-                        provider.name.toLowerCase() === 'google'
-                          ? '/icons/profile/auth-google.svg'
-                          : '/icons/profile/auth-github-mark.svg'
+                        // Otherwise if a redirects happens Next.js can handle it
+                        // so you can just re-thrown the error and let Next.js handle it.
+                        // Docs:
+                        // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
+                        throw error
                       }
-                      alt={provider.name}
-                      width={20}
-                      height={20}
-                    />
-                  }
-                >
-                  <Typography>Sign in with {provider.name}</Typography>
-                </Button>
-              </Paper>
-            ))}
-            <CredentialsInput />
-          </Stack>
-          <Stack alignItems={'center'} sx={{ mt: 'auto' }}>
+                    }}
+                    elevation={0}
+                    sx={{ backgroundColor: 'transparent' }}
+                  >
+                    <Button
+                      type="submit"
+                      variant="outlined"
+                      sx={{ m: 2, borderRadius: '12px' }}
+                      startIcon={
+                        <Image
+                          src={
+                            provider.name.toLowerCase() === 'google'
+                              ? '/icons/profile/auth-google.svg'
+                              : '/icons/profile/auth-github-mark.svg'
+                          }
+                          alt={provider.name}
+                          width={20}
+                          height={20}
+                        />
+                      }
+                    >
+                      <Typography>Sign in with {provider.name}</Typography>
+                    </Button>
+                  </Paper>
+                ))}
+                <CredentialsInput />
+              </Stack>
+            )}
+
             {!session && (
               <Stack spacing={1}>
                 <Typography variant="body1">
                   Don&apos;t have an account yet?
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                  ✨ Just enter your email below - if it&apos;s new, we&apos;ll
-                  help you create an account!
+                  ✨ Use any method to login the first timea and an account will
+                  be created for you!
                 </Typography>
               </Stack>
             )}
           </Stack>
-          {session && (
-            <Paper
-              component="form"
-              action={async () => {
-                'use server'
-                await signOut({ redirect: true, redirectTo: '/auth/signout' })
-              }}
-              elevation={0}
-              sx={{ backgroundColor: 'transparent' }}
-            >
-              <Button variant="contained" type="submit" sx={{ width: '200px' }}>
-                Sign out
-              </Button>
-            </Paper>
-          )}
         </Stack>
       </Stack>
     </>
